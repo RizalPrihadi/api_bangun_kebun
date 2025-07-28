b6e0546c27214435d17ccabbb5a2f7296bc56c93
RizalPrihadi <rizalprihadi25@gmail.com>
2025-07-28 19:11:54 +0700

    Add project files.

---
+++ api_bangun_kebun/Helpers/JwtHelper.cs
@@ -0,0 +1,35 @@
+﻿//using Microsoft.IdentityModel.Tokens;
+//using System.IdentityModel.Tokens.Jwt;
+//using System.Security.Claims;
+//using System.Text;
+
+//namespace api_bangun_kebun.Helpers
+//{
+//    public class JwtHelper
+//    {
+//        private readonly IConfiguration _config;
+//        public JwtHelper(IConfiguration config)
+//        {
+//            _config = config;
+//        }
+
+//        public string GenerateToken(Pengguna pengguna)
+//        {
+//            var tokenHandler = new JwtSecurityTokenHandler();
+//            var key = Encoding.ASCII.GetBytes(_config["Jwt:Key"]);
+//            var tokenDescriptor = new SecurityTokenDescriptor
+//            {
+//                Subject = new ClaimsIdentity(new[]
+//                {
+//                    new Claim("id_pengguna", pengguna.id_pengguna.ToString()),
+//                    new Claim(ClaimTypes.Email, pengguna.email),
+//                    new Claim(ClaimTypes.Role, pengguna.nama_peran)
+//                }),
+//                Expires = DateTime.UtcNow.AddHours(1),
+//                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+//            };
+//            var token = tokenHandler.CreateToken(tokenDescriptor);
+//            return tokenHandler.WriteToken(token);
+//        }
+//    }
+//}
---
+++ api_bangun_kebun/Helpers/SqlDbHelper.cs
@@ -0,0 +1,33 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace api_bangun_kebun.Helpers
+{
+    public class SqlDbHelper
+    {
+        private NpgsqlConnection connection;
+        private string __constr;
+
+        public SqlDbHelper(string pConstr)
+        {
+            __constr = pConstr;
+            connection = new NpgsqlConnection();
+            connection.ConnectionString = __constr;
+        }
+
+        public NpgsqlCommand GetNpgsqlCommand(string query)
+        {
+            connection.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = query;
+            cmd.CommandType = CommandType.Text;
+            return cmd;
+        }
+
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
+    }
+}
