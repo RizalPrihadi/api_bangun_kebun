657e740f0c237cdd50603a662cc7240fe0995daf
Stwn25 <msetiawanwibisono@gmail.com>
2025-07-30 22:24:45 +0700

    feat: add content and other context

---
+++ api_bangun_kebun/Contexts/ContentContext.cs
@@ -0,0 +1,48 @@
+﻿using api_bangun_kebun.Models;
+using api_bangun_kebun.Helpers;
+using Npgsql;
+
+namespace api_bangun_kebun.Contexts
+{
+    public class ContentContext
+    {
+        private string _constr;
+        private string _errMsg;
+
+        public ContentContext(string conn) {
+            _constr = conn;
+        }
+
+        public bool createContent(Content content)
+        {
+            SqlDbHelper db = new SqlDbHelper(this._constr);
+
+            string query = @"insert into konten(pengguna_id_user, gambar_video, isi_deskripsi, timestamp, jenis_konten_id_jenis_konten, like) 
+                            values(@id_pengguna, @gambar_video, @isi_deskripsi, @timestamp, @id_jenis_konten, 0)";
+
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                cmd.Parameters.AddWithValue("@id_pengguna", content.id_user!);
+                cmd.Parameters.AddWithValue("@gambar_video", content.gambar_video!);
+                cmd.Parameters.AddWithValue("@isi_deskripsi", content.isi_deskripsi!);
+                cmd.Parameters.AddWithValue("@timestamp", content.timestamp!);
+                cmd.Parameters.AddWithValue("@id_jenis_konten", content.id_jenis_konten!);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                cmd.Dispose();
+                db.closeConnection();
+
+                return rowsAffected > 0;
+            }
+            catch(Exception ex)
+            {
+                _errMsg = ex.Message;
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ api_bangun_kebun/Contexts/OtherContext.cs
@@ -0,0 +1,48 @@
+﻿using api_bangun_kebun.Helpers;
+using Npgsql;
+
+namespace api_bangun_kebun.Contexts
+{
+    public class OtherContext
+    {
+        private string _constr;
+        private string _errMsg;
+
+        public OtherContext(string conn)
+        {
+            _constr = conn;
+        }
+
+        public List<dynamic> getDataJenisKonten()
+        {
+            List<dynamic> data = new List<dynamic>();
+
+            string query = "select * from jenis_konten";
+
+            SqlDbHelper db = new SqlDbHelper(this._constr);
+
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    data.Add(new
+                    {
+                        id_jenis_konten = int.Parse(reader["id_jenis_konten"].ToString()),
+                        jenis_konten = reader["jenis_konten"].ToString()
+                    });
+                }
+
+                cmd.Dispose();
+                db.closeConnection();
+            }catch(Exception ex)
+            {
+                _errMsg = ex.Message;
+            }
+
+            return data;
+        }
+    }
+}
