93945eb20f79af7a902bf31166c1a62be9fc7850
RizalPrihadi <rizalprihadi25@gmail.com>
2025-07-30 22:48:30 +0700

    feat: add ProductController

---
+++ api_bangun_kebun/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_bangun_kebun.Controllers
+{
+    [ApiController]
+    [Route("bangunKebun/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        public readonly string __constr;
+        private readonly IConfiguration __config;
+        public ProductController(IConfiguration configuration)
+        {
+            __config = configuration;
+            __constr = __config.GetConnectionString("koneksi");
+        }
+    }
+}
