6c0ea707dead1310982247f36c3eb4d53aba49c3
Stwn25 <msetiawanwibisono@gmail.com>
2025-07-30 22:26:26 +0700

    feat: add model content

---
+++ api_bangun_kebun/Models/Content.cs
@@ -0,0 +1,13 @@
+﻿namespace api_bangun_kebun.Models
+{
+    public class Content
+    {
+        public int? id_content { get; set; }
+        public int? id_user { get; set; }
+        public int? id_jenis_konten { get; set; }
+        public string? gambar_video { get; set; }
+        public string? isi_deskripsi { get; set; }
+        public DateTime? timestamp { get; set; }
+        public int? like {  get; set; }
+    }
+}
