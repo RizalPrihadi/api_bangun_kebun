3b992342b0db6489111c25d616c77b9c4667af62
RizalPrihadi <rizalprihadi25@gmail.com>
2025-08-02 13:03:55 +0700

    feat: add ProductContext

--- api_bangun_kebun/Contexts/ProductContext.cs
+++ api_bangun_kebun/Contexts/ProductContext.cs
@@ -31,13 +31,13 @@ namespace api_bangun_kebun.Contexts
                     product.Add(new Product
                     {
                         id_user = int.Parse(reader["id_user"].ToString()),
-                        id_product = int.Parse(reader["id_product"].ToString()),
+                        id_produk = int.Parse(reader["id_produk"].ToString()),
                         nama_produk = reader["nama_produk"].ToString(),
                         gambar_produk = reader["gambar_produk"].ToString(),
                         harga = double.Parse(reader["harga"].ToString()),
                         timestamp = DateTime.Parse(reader["timestamp"].ToString()),
-                        id_jenis_produk = int.Parse(reader["id_jenis_produk"].ToString()),
-                        id_status_ketahanan = int.Parse(reader["id_status_ketahanan"].ToString())
+                        id_jenis_produk = int.Parse(reader["jenis_produk_id_jenis_produk"].ToString()),
+                        id_status_ketahanan = int.Parse(reader["status_ketahanan_id_status_ketahanan"].ToString())
                     });
                 }
 
@@ -50,6 +50,219 @@ namespace api_bangun_kebun.Contexts
             }
 
             return product;
+        }
+
+        public List<Product> GetDataProductById(int id_produk)
+        {
+            List<Product> dataProduct = new List<Product>();
+
+            string query = @"SELECT * FROM produks WHERE id_produk = @id_produk";
+
+            SqlDbHelper db = new SqlDbHelper(this._constr);
+
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                cmd.Parameters.AddWithValue("@id_produk", id_produk);
+
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    dataProduct.Add(new Product
+                    {
+                        id_user = int.Parse(reader["pengguna_id_user"].ToString()),
+                        id_produk = int.Parse(reader["id_produk"].ToString()),
+                        nama_produk = reader["nama_produk"].ToString(),
+                        gambar_produk = reader["gambar_produk"].ToString(),
+                        harga = double.Parse(reader["harga"].ToString()),
+                        timestamp = DateTime.Parse(reader["timestamp"].ToString()),
+                        id_jenis_produk = int.Parse(reader["jenis_produk_id_jenis_produk"].ToString()),
+                        id_status_ketahanan = int.Parse(reader["status_ketahanan_id_status_ketahanan"].ToString())
+                    });
+                }
+
+                cmd.Dispose();
+                db.closeConnection();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Get product gagal: " + ex.Message);
+            }
+
+            return dataProduct;
+        }
+
+        public List<Product> GetDataProductByUser(string nama_lengkap)
+        {
+            List<Product> dataProduct = new List<Product>();
+
+            string query = @"SELECT * FROM produks
+                            JOIN pengguna ON produks.pengguna_id_pengguna = pengguna.id_pengguna
+                            WHERE nama_lengkap = @nama_lengkap";
+
+            SqlDbHelper db = new SqlDbHelper(this._constr);
+
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                cmd.Parameters.AddWithValue("@nama_lengkap", nama_lengkap);
+
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    dataProduct.Add(new Product
+                    {
+                        id_user = int.Parse(reader["pengguna_id_user"].ToString()),
+                        id_produk = int.Parse(reader["id_produk"].ToString()),
+                        nama_produk = reader["nama_produk"].ToString(),
+                        gambar_produk = reader["gambar_produk"].ToString(),
+                        harga = double.Parse(reader["harga"].ToString()),
+                        timestamp = DateTime.Parse(reader["timestamp"].ToString()),
+                        id_jenis_produk = int.Parse(reader["jenis_produk_id_jenis_produk"].ToString()),
+                        id_status_ketahanan = int.Parse(reader["status_ketahanan_id_status_ketahanan"].ToString())
+                    });
+                }
+
+                cmd.Dispose();
+                db.closeConnection();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Get product gagal: " + ex.Message);
+            }
+
+            return dataProduct;
+        }
+
+        public List<Product> GetDataProductByJenis(string nama_jenis_product)
+        {
+            List<Product> dataProduct = new List<Product>();
+
+            string query = @"SELECT * FROM produks
+                            JOIN jenis_produk ON produks.jenis_produk_id_jenis_produk = jenis_produk.id_jenis_product
+                            WHERE nama_jenis_product = @nama_jenis_product";
+
+            SqlDbHelper db = new SqlDbHelper(this._constr);
+
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                cmd.Parameters.AddWithValue("@nama_jenis_product", nama_jenis_product);
+
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    dataProduct.Add(new Product
+                    {
+                        id_user = int.Parse(reader["pengguna_id_user"].ToString()),
+                        id_produk = int.Parse(reader["id_produk"].ToString()),
+                        nama_produk = reader["nama_produk"].ToString(),
+                        gambar_produk = reader["gambar_produk"].ToString(),
+                        harga = double.Parse(reader["harga"].ToString()),
+                        timestamp = DateTime.Parse(reader["timestamp"].ToString()),
+                        id_jenis_produk = int.Parse(reader["jenis_produk_id_jenis_produk"].ToString()),
+                        id_status_ketahanan = int.Parse(reader["status_ketahanan_id_status_ketahanan"].ToString())
+                    });
+                }
+
+                cmd.Dispose();
+                db.closeConnection();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Get product gagal: " + ex.Message);
+            }
+
+            return dataProduct;
+        }
+
+        public bool CreateProduct(Product product)
+        {
+            SqlDbHelper db = new SqlDbHelper(this._constr);
+
+            string query = @"INSERT INTO produks (pengguna_id_user, nama_produk, gambar_produk, harga, timestamp, jenis_produk_id_jenis_produk, status_ketahanan_id_status_ketahanan)
+                            VALUES(@id_user, @nama_produk, @gambar_produk, @harga, @timestamp, @id_jenis_produk, @id_status_ketahanan)";
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                cmd.Parameters.AddWithValue("@id_user", product.id_user);
+                cmd.Parameters.AddWithValue("@nama_produk", product.nama_produk);
+                cmd.Parameters.AddWithValue("@gambar_produk", product.gambar_produk);
+                cmd.Parameters.AddWithValue("@harga", product.harga);
+                cmd.Parameters.AddWithValue("@timestamp", product.timestamp);
+                cmd.Parameters.AddWithValue("@id_jenis_produk", product.id_jenis_produk);
+                cmd.Parameters.AddWithValue("@id_status_ketahanan", product.id_status_ketahanan);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                cmd.Dispose();
+                db.closeConnection();
+
+                return rowsAffected > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Create product gagal: " + ex.Message);
+            }
+        }
+
+        public bool UpdateProduct(int id_produk, Product product)
+        {
+            SqlDbHelper db = new SqlDbHelper(this._constr);
+
+            string query = @"UPDATE produks 
+                            SET nama_produk = @nama_produk, 
+                                gambar_produk = @gambar_produk, 
+                                harga = @harga, 
+                                timestamp = @timestamp, 
+                                jenis_produk_id_jenis_produk = @id_jenis_produk, 
+                                status_ketahanan_id_status_ketahanan = @id_status_ketahanan
+                            WHERE id_produk = @id_produk";
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                cmd.Parameters.AddWithValue("@id_produk", product.id_produk);
+                cmd.Parameters.AddWithValue("@nama_produk", product.nama_produk);
+                cmd.Parameters.AddWithValue("@gambar_produk", product.gambar_produk);
+                cmd.Parameters.AddWithValue("@harga", product.harga);
+                cmd.Parameters.AddWithValue("@timestamp", product.timestamp);
+                cmd.Parameters.AddWithValue("@id_jenis_produk", product.id_jenis_produk);
+                cmd.Parameters.AddWithValue("@id_status_ketahanan", product.id_status_ketahanan);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                cmd.Dispose();
+                db.closeConnection();
+
+                return rowsAffected > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Update product gagal: " + ex.Message);
+            }
+        }
+
+        public bool DeleteProduct(int id_produk)
+        {
+            SqlDbHelper db = new SqlDbHelper(this._constr);
+
+            string query = @"DELETE FROM produks WHERE id_produk = @id_produk";
+            try
+            {
+                NpgsqlCommand cmd = db.GetNpgsqlCommand(query);
+                cmd.Parameters.AddWithValue("@id_produk", id_produk);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                cmd.Dispose();
+                db.closeConnection();
+
+                return rowsAffected > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Delete product gagal: " + ex.Message);
+            }
         }
     }
 }
